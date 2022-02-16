4a076613e474aad477abafa516d060f56e99faa0
Oleksii Ponomarov <rocknrollerr@gmail.com>
2022-02-16 20:38:27 +0200

    fixed geometry bug

--- src/objects/markers.js
+++ src/objects/markers.js
@@ -3,6 +3,9 @@ import * as THREE from "three";
 import { convertLatLongToXYZ } from "../utils";
 import { getSunLong, getSunLat } from "./light";
 
+const markerGeometry = new THREE.SphereGeometry(0.08, 30, 30);
+const markerMaterial = new THREE.MeshBasicMaterial({ color: "#FF0000" });
+
 const sunMarker = new THREE.Mesh(markerGeometry, markerMaterial);
 const sunMarkerPosition = convertLatLongToXYZ(10, getSunLat(), getSunLong());
 sunMarker.position.x = sunMarkerPosition.x;
